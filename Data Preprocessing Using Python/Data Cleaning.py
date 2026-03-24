af434230f5783cf5916f445834104120b405fcb1
Raju Sharma <95718832+Raju-Sharma1@users.noreply.github.com>
2026-03-24 19:15:34 +0530

    Update Data Cleaning.py

--- Data Preprocessing Using Python/Data Cleaning.py
+++ Data Preprocessing Using Python/Data Cleaning.py
@@ -1,46 +1,58 @@
 import pandas as pd
-import numpy as np
 
 
-# Cleaning Customers Tables Data:
-file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project (Raju Sharma)/EDA (Exploratory Data Analytics Project)/Testing with the gold layer tables/gold.dim_customers.csv"
+#                             Cleaning Customers Tables Data:
+#                             xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project/gold.dim_customers.csv"
 
 dim_customers = pd.read_csv(file)
 print(dim_customers)
 dim_customers.isnull().sum()
 
 # Handling missing values.
+#=========================
+
 dim_customers["country"] = dim_customers["country"].fillna(dim_customers["country"].mode()[0])
 dim_customers["gender"] = dim_customers["gender"].fillna(dim_customers["gender"].mode()[0])
 dim_customers["birthdate"] = dim_customers["birthdate"].fillna("Unknown")
 
-# Updating the csv file
+# Updating the csv file.
+#=======================
 dim_customers.to_csv("Cleaned_gold.dim_customers.csv", index=False)
-#=======================================================================
+#=======================================================================================================
+#=======================================================================================================
 
-# Cleaning Products Table Data:
-file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project (Raju Sharma)/EDA (Exploratory Data Analytics Project)/Testing with the gold layer tables/gold.dim_products.csv"
+#                             Cleaning Products Table Data:
+#                             xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project/gold.dim_products.csv"
 
 dim_products = pd.read_csv(file)
 dim_products
 dim_products.isnull().sum()
 
 # Handling null values.
+#======================
 dim_products = dim_products.fillna("Unknown")
 
-# Updating the csv file
+# Updating the csv file.
+#=======================
 dim_products.to_csv("Cleaned_gold.dim_products.csv", index=False)
-#=======================================================================
+#=======================================================================================================
+#=======================================================================================================
 
-# Cleaning Sales Tables Data:
+#                             Cleaning Sales Tables Data:
+#                             xxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project (Raju Sharma)/EDA (Exploratory Data Analytics Project)/Testing with the gold layer tables/gold.fact_sales2.csv"
+file = "C:/Users/rajsh/OneDrive/Desktop/SQL Project/Data Analytics Project/gold.fact_sales2.csv"
 
 fact_sales = pd.read_csv(file)
 
 fact_sales.isnull().sum()
 
 # Handling null vales.
+#=====================
 fact_sales["order_date"] = pd.to_datetime(fact_sales["order_date"])
 fact_sales["shipping_date"] = pd.to_datetime(fact_sales["shipping_date"])
 fact_sales["due_date"] = pd.to_datetime(fact_sales["due_date"])
@@ -48,13 +60,17 @@ fact_sales["order_date"] = fact_sales["order_date"].fillna(fact_sales["shipping_
 fact_sales["order_date"] = str(fact_sales["order_date"])
 fact_sales["shipping_date"] = str(fact_sales["shipping_date"])
 
-# checking if the date was filled correctly in the missing columns:
+# checking if the date was filled correctly in the missing columns.
+#==================================================================
 fact_salesfiltered = fact_sales[fact_sales["order_number"] == "SO64338"]
 fact_salesfiltered[["order_date", "shipping_date"]]
 fact_sales.dtypes
 
 fact_sales[["order_date", "shipping_date", "due_date"]]
 
-# Updating the csv file
+# Updating the csv file.
+#=======================
 fact_sales.to_csv("Cleaned_gold.fact_sales.csv", index=False)
-#=======================================================================
+
+#=======================================================================================================
+#=======================================================================================================
